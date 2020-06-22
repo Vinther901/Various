3b2a18d0150da254264fc8bd9c63f6b753b00ee1
Vinther901 <jv97@live.dk>
2020-06-22 22:22:32 +0200

    Tweaked 'search' still not working

--- ConnectFour.py
+++ ConnectFour.py
@@ -30,9 +30,9 @@ player = 1
 #     #     moveable.pop
 #     moveable[column][0] -= 1
 
-def moveables(board):
+def moveablesFunc(board):
     heights = np.sum(board != 0, axis=0)
-    return [(5 - h,i) for i,h in enumerate(heights) if 5-h > 0]
+    return [(5 - h,i) for i,h in enumerate(heights) if 5-h >= 0]
 
 
 def scoreFunc(pos,player,board):
@@ -62,16 +62,95 @@ def scoreFunc(pos,player,board):
         fours.append(diag1[d1loc-i:d1loc-i+4])
         fours.append(diag2[d2loc-i:d2loc-i+4])
     
-    # stwos = list(map(sum,twos))
-    # sthrees = list(map(sum,threes))
-    # sfours = list(map(sum,fours))
+    stwos = list(map(sum,twos))
+    sthrees = list(map(sum,threes))
+    sfours = list(map(sum,fours))
 
-    stwo = sum(np.asarray(list(map(sum,twos))) == 2*player)
-    sthree = sum(np.asarray(list(map(sum,threes))) == 3*player)
-    sfour = sum(np.asarray(list(map(sum,fours))) == 4*player)
+    stwo = sum(np.asarray(stwos) == 2*player)
+    sthree = sum(np.asarray(sthrees) == 3*player)
+    sfour = sum(np.asarray(sfours) == 4*player)
 
-    score = player*(12*sfour + 6*sthree + 3*stwo)
+    score = player*(20*sfour + 6*sthree + 3*stwo)
     return score
+
+
+def search(player,depth):
+    moveables = moveablesFunc(board)
+    for pos in moveables:
+        board[pos[0],pos[1]] = player
+        print(pos, search2(pos,player,depth))
+        board[pos[0],pos[1]] = 0
+
+def search2(pos,player,depth,alpha=-np.inf,beta=np.inf):
+    moveables = moveablesFunc(board)
+
+    if depth <= 0 or len(moveables) == 0:
+        # score = scoreFunc(pos,-player,board)
+        score = 0
+        for row, col in zip(yy[board == -player],xx[board == -player]):
+            score += scoreFunc((row,col),-player,board)
+        for row, col in zip(yy[board == player],xx[board == player]):
+            score += scoreFunc((row,col),player,board)
+        return score
+    
+    maximizing = player == 1
+
+    if maximizing:
+        value = -np.inf
+        for pos in moveables:
+            board[pos[0],pos[1]] = player
+            value = max(value,search2(pos,-player,depth-1,alpha,beta))
+            board[pos[0],pos[1]] = 0
+            alpha = max(alpha,value)
+            if alpha >= beta:
+                # print('alpha break')
+                break
+        return value
+    else:
+        value = np.inf
+        for pos in moveables:
+            board[pos[0],pos[1]] = player
+            value = min(value,search2(pos,-player,depth-1,alpha,beta))
+            board[pos[0],pos[1]] = 0
+            beta = min(beta,value)
+            if beta <= alpha:
+                # print('beta break')
+                break
+        return value
+
+
+def onclick(event):
+    global player
+    moveables = np.array(moveablesFunc(board))
+    col = int(event.xdata + 0.5)
+    pos = moveables[moveables[:,1] == col][0]
+    try:
+        board[pos[0],pos[1]] = player
+    except:
+        print("Invalid move")
+        return
+
+    yellow.set_offsets(np.array([xx[board == 1],yy[board == 1]]).T)
+    red.set_offsets(np.array([xx[board == -1], yy[board == -1]]).T)
+
+    player *= -1
+    ax.set_title(f"player {player}'s turn. 1: Yellow, -1: Red")
+
+    fig.canvas.draw()
+
+
+fig, ax = plt.subplots(figsize=(8,8))
+ax.set(xlim=(-1,7),ylim=(-1,6),title=f"player {player}'s turn. 1: Yellow, -1: Red")
+plt.gca().invert_yaxis()
+radius = 2000
+
+plate = ax.scatter(xx,yy,s=radius)
+yellow = ax.scatter([],[],s=radius,c='yellow')    #player 1
+red = ax.scatter([],[],s=radius,c='r')            #player -1
+
+fig.show()
+
+fig.canvas.mpl_connect('button_press_event',onclick)
 
 # def search(pos,player,depth,alpha,beta,moveable):
 #     # print(board)
@@ -110,33 +189,3 @@ def scoreFunc(pos,player,board):
 #                 break
             
 #         return value
-
-def search2(pos,player,depth,board):
-    if depth <= 0:
-        return scoreFunc(pos,player,board)
-
-
-def onclick(event):
-    global player
-    score = move(event.xdata,player,moveable)
-    yellow.set_offsets(np.array([xx[board == 1],yy[board == 1]]).T)
-    red.set_offsets(np.array([xx[board == -1], yy[board == -1]]).T)
-
-    player *= -1
-    ax.set_title(f"player {player}'s turn. 1: Yellow, -1: Red")
-
-    fig.canvas.draw()
-
-
-fig, ax = plt.subplots(figsize=(8,8))
-ax.set(xlim=(-1,7),ylim=(-1,6),title=f"player {player}'s turn. 1: Yellow, -1: Red")
-plt.gca().invert_yaxis()
-radius = 2000
-
-plate = ax.scatter(xx,yy,s=radius)
-yellow = ax.scatter([],[],s=radius,c='yellow')    #player 1
-red = ax.scatter([],[],s=radius,c='r')            #player -1
-
-fig.show()
-
-fig.canvas.mpl_connect('button_press_event',onclick)
